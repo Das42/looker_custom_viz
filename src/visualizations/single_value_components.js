47cd89d29048f9328d1f44b66a16899d1b61baec
Jordan Hembling <jordan.hembling@colorado.edu>
2020-12-29 16:01:50 -0700

    adds comparison functionality and additonal opptions

--- src/visualizations/single_value_components.js
+++ src/visualizations/single_value_components.js
@@ -9,8 +9,14 @@ export class SingleValueVis extends React.Component {
     render() {
       return <div>
         {<SingleValue html_formatted={this.props.html_formatted}/>}
-        {this.props.show_comparison && <Comparison comparison={this.props.comparison}/>}
         {this.props.show_title && <Title title={this.props.title}/>}
+        {this.props.show_comparison && 
+          <Comparison 
+            comparison={this.props.comparison}
+            show_comparison_label={this.props.show_comparison_label}
+            comparison_label={this.props.comparison_label}
+            comparison_value_label={this.props.comparison_value_label}
+          />}
       </div>
     }
   }
@@ -37,9 +43,37 @@ class Title extends React.Component {
 class Comparison extends React.Component {
   constructor (props) {
     super(props)
+    this.comparisonChangeIndicator = this.comparisonChangeIndicator.bind(this)
+  }
+
+  comparisonChangeIndicator () {
+    const comp_value = parseFloat(this.props.comparison.replace('−', '-'))
+    const comp_rendered = this.props.comparison.replace(/−|-/g, '')
+    if (comp_value < 0) {
+      return  <span style={{color: 'red'}}>{'\u2207' + ' ' + comp_rendered + ' '}</span>
+    } else {
+      return  <span style={{color: 'green'}}>{'\u2206' + ' ' + comp_rendered + ' '}</span>
+            
+    }
   }
 
   render() {
-    return <div className='comp' id='comparison'> {this.props.comparison} </div>
+    return <div className='comp' id='comparison'> 
+      {
+        this.props.show_comparison_label && this.props.comparison_value_label === 'show_value' ?
+          <div className='comp'> 
+            {this.props.comparison + ' ' + this.props.comparison_label} 
+          </div>
+        :
+        this.props.comparison_value_label === 'show_change' ?
+            <span>
+              {this.comparisonChangeIndicator()} 
+              {this.props.show_comparison_label && this.props.comparison_label}
+            </span>
+        : 
+        <div className='comp'> 
+          {this.props.comparison} 
+        </div>} 
+    </div>
   }
 }
--- src/visualizations/single_value_viz.tsx
+++ src/visualizations/single_value_viz.tsx
@@ -40,6 +40,20 @@ const vis = {
       type: 'boolean',
       label: 'Show Comparison',
       default: false
+    },
+    show_comparison_label: {
+      section: 'Comparison',
+      type: 'boolean',
+      label: 'Show Label',
+      default: true
+    },
+    comparison_value_label: {
+      section: 'Comparison',
+      type: 'string',
+      display: 'select',
+      label: 'Value Labels',
+      values: [{'Show as Value': 'show_value'}, {'Show as Change': 'show_change'}],
+      default: 'show_change'
     }
   },
 
@@ -52,40 +66,23 @@ const vis = {
         flex-direction: column;
         justify-content: center;
         text-align: center;
+        flex: 1 1 auto; 
+        padding: 5px;
       }
 
       .single-value {
-        height: 50%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        text-align: center;
-        font-size: 52px;
+        font-size: 32px;
+        flex: 1 0 0;
         word-wrap: break-word;
-        padding: 5px;
-        margin: 5px;
       }     
 
       .title {
-        height: 25%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        text-align: center;
-        font-size: 36px;
         word-wrap: break-word;
-        padding: 5px; 
-        margin: 20px;
+        margin-bottom: 20px;
+        margin-top: -20px;
       }
 
       .comp {
-        height: 25%;
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        text-align: center;
-        font-size: 36px;
-        word-wrap: break-word;
       }
       </style>
     `
@@ -108,18 +105,38 @@ const vis = {
 
     const qFields = queryResponse.fields
 
-    function formatCell(row, column) {
-      try {
+    function getRow(row) {
+      return data[row]
+    }
 
-        const row_detail = data[row]
-        const return_field = qFields.dimension_like.length > 0 ? qFields.dimension_like[column] : qFields.measure_like[column]
-        const cell_name = row_detail[return_field.name]
-        const cell_value = LookerCharts.Utils.filterableValueForCell(cell_name)
+    function getColumn(column, object) {
+      if (object === 'comp') {
+        return qFields.dimension_like.length > 0 ? qFields.dimension_like[column] : qFields.measure_like[column]
+      } else {
+          return qFields.measure_like.length > 0 ? qFields.measure_like[column] : qFields.dimension_like[column]
+      }
+    }
+
+    function getCellName(row, column, object) {
+      const row_detail = getRow(row)
+      const column_detail = getColumn(column, object)
+      return row_detail[column_detail.name]      
+    }
+
+    function getCellValue(cell_name) {
+      return LookerCharts.Utils.filterableValueForCell(cell_name)
+    }
+
+    function formatCell(row, column, object) {
+      try {
+        const cell_name = getCellName(row, column, object)
+        const column_detail = getColumn(column, object)
+        const cell_value = getCellValue(cell_name)
 
         // create default format for measures or table calcs with no value_format set
         let cell_format = ""
-        if (return_field.category === "measure" && qFields.measure_like[column].value_format === null 
-            || return_field.is_table_calculation && return_field.value_format === null) {
+        if ((column_detail.category === "measure"  || column_detail.is_table_calculation ) 
+            && column_detail.value_format === null) {
           const measure_value = parseFloat(cell_value)
           if(Math.round(measure_value) !== measure_value) {
             cell_format = "#,##0.00"
@@ -127,11 +144,11 @@ const vis = {
             cell_format = "#,##0"
            }
         } else {
-           cell_format = return_field.value_format
+           cell_format = column_detail.value_format
         }
 
         const formatValue = formatType(cell_format) || defaultFormatter
-        console.log(cell_value)
+
         return formatValue(cell_value).replace(/^"(.*)"$/, '$1')
         }
       catch (e) {
@@ -139,20 +156,21 @@ const vis = {
         }
     }
 
-    const comparison = formatCell(0,1) ? formatCell(0,1) : formatCell(1,0)
+    function formatHTML() {
+      return { __html: htmlTemplate.replace(/{{.*}}/g, firstCellFormatted) }
+    }
+
+    function componentHTML() {
+      return <div dangerouslySetInnerHTML={formatHTML()} />
+    }
 
     function getComparison() {
-      let i = 0
-      // console.log(i)
-      // return formatCell(0,1) ? formatCell(0,1) : formatCell(1,0)
-      // if (formatCell(0,1)) {
-      //   return formatCell(1,2)
-      // } else if (formatCell(2,1).length > 0) {
-      //   return formatCell(2,1)
-      // } else return this.addError({
-      //   title: `No fields available for comp.`,
-      //   message: `At least one dimension, measure or table calculation needs to be visible.`
-      // })
+      const comp_field = getColumn(0, 'comp')
+      if (comp_field.category === 'dimension') {
+        return formatCell(0,0,'comp') === firstCellFormatted ? [getColumn(1, 'comp'), formatCell(0,1,'comp')] : [getColumn(0,'comp'), formatCell(0,0,'comp')]
+      } else {
+        return formatCell(0,1,'comp') ? [getColumn(1, 'comp'), formatCell(0,1,'comp')] : [getColumn(0, 'comp'), formatCell(1,0,'comp')]
+      }
     }
 
     if (qFields.dimension_like.length === 0 &&
@@ -162,25 +180,24 @@ const vis = {
         message: `At least one dimension, measure or table calculation needs to be visible.`
       })
     }
-
-    const firstCell = formatCell(0, 0)
+    
+    const firstCellFormatted = formatCell(0, 0, 'sv')
     const htmlTemplate = config && config.html_template || this.options.html_template.default
 
-    function formatHTML() {
-      return { __html: htmlTemplate.replace(/{{.*}}/g, firstCell) }
-    }
-
-    function componentHTML() {
-      return <div dangerouslySetInnerHTML={formatHTML()} />
-    }
+    const comparison = getComparison()
 
     this.chart = ReactDOM.render(
       <SingleValueVis
         title={config.title}
         show_title={config.show_title}
         show_comparison={config.show_comparison}
+        show_comparison_label={config.show_comparison_label}
         html_formatted={componentHTML()}
-        comparison={comparison}/>,
+        comparison={comparison[1]}
+        comparison_label={comparison[0].label}
+        comparison_value_label={config.comparison_value_label}
+        />,
+        
       document.getElementById('vis-container')
     )
 
